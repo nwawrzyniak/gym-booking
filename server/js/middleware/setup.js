9c46408dbdc30578044c115922e953ecd4a9b544
Nico Wawrzyniak <nwawrzyniak@immopark.de>
2026-02-03 11:55:11 +0100

    Fixes

--- server/js/middleware/setup.js
+++ server/js/middleware/setup.js
@@ -5,7 +5,7 @@ const sessionConfig = require('../config/session');
 
 const setupMiddleware = (app) => {
   app.set('view engine', 'ejs');
-  app.set('views', path.join(__dirname, '..', 'views'));
+  app.set('views', path.join(__dirname, '../../../views'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.static('public'));
